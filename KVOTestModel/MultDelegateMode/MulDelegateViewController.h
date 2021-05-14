d5c126ceec2355814f4cb1a3c919a3f7f87c472e
zhangjiang <zhangj@ibobei.cn>
2021-05-14 10:41:28 +0800

    添加多代理方法

---
+++ KVOTestModel/MultDelegateMode/MulDelegateViewController.h
@@ -0,0 +1,17 @@
+//
+//  MulDelegateViewController.h
+//  KVOTestModel
+//
+//  Created by zhangjiang on 2021/5/14.
+//  Copyright © 2021 zhangjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MulDelegateViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
